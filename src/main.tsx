c7ab767c967f3363808f6a873156afc6901dead7
HansiniAbyegunawardhana <hchathupomoda@gmail.com>
2024-03-30 02:47:57 +0530

    Add Redux Persist for state persistence

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './state/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
     <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counter/counterSlice";
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from "redux-persist";
+
+// export const store = configureStore({
+//     reducer: {
+//         counter : counterSlice
+//     },
+// })
+
+const rootReducer = combineReducers({
+    counter : counterSlice
+})
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        counter : counterSlice
-    },
+    reducer: persistedReducer
 })
 
 export type AppDispatch = typeof store.dispatch
